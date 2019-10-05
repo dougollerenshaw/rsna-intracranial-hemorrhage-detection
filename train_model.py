ad108ddc5b40d7e4cdbdd979e3c3c10c6914ba2d
Matt Valley <valley@gmail.com>
2019-10-05 12:09:37 -0700

    making it classey

--- train_model.py
+++ train_model.py
@@ -4,7 +4,6 @@ import pandas as pd
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-from tqdm import tqdm as progress
 from skimage import exposure, io, transform
 from skimage.transform import rotate, warp
 from skimage.transform import SimilarityTransform
@@ -30,86 +29,103 @@ import tensorflow as tf
 import utils
 from models import models
 
-def make_y_image(generator,model,filename):
-    generator.__reset__()
-    X,y=generator.__next__()
 
-    y_pred = model.predict_proba(X)
-    fig,ax=plt.subplots(1,2,figsize=(10,10))
-    ax[0].imshow(y,aspect='auto')
-    ax[1].imshow(y_pred,aspect='auto')
-    ax[0].set_title('y_actual')
-    ax[1].set_title('y_pred')
-    fig.savefig(filename)
+class rsna_model(object):
 
-def main(dataloc = r'D:\rsna-intracranial-hemorrhage-detection', 
-        model_name = 'vgg',
-        training_fraction = 0.15,
-        batch_size = 16,
-        img_size = 256,
-        rgb = False,
-        ):
+    def __init__(self,
+                dataloc = r'D:\rsna-intracranial-hemorrhage-detection', 
+                weights_path = None,
+                model_name = 'vgg',
+                training_fraction = 0.15,
+                batch_size = 16,
+                img_size = 256,
+                epochs = 1,
+                rgb = False
+                ):
 
-    # load training df
-    tdf = utils.load_training_data(dataloc)
+        self.dataloc = dataloc
+        self.weights_path = weights_path
+        self.model_name = model_name
+        self.training_fraction = training_fraction
+        self.batch_size = batch_size
+        self.img_size = img_size
+        self.epochs = epochs
+        self.rgb = rgb
 
-    # set up training fraction
-    ## train and validate dataframes
-    shuff = tdf.sample(frac=training_fraction)
-    train_df = shuff.iloc[:int(0.90*len(shuff))]
-    validate_df = shuff.iloc[int(0.10*len(shuff)):]
-    len(shuff),len(train_df),len(validate_df)
+    def make_y_image(self,generator,model,filename):
+        generator.__reset__()
+        X,y=generator.__next__()
 
-    # set up generators
-    categories = utils.define_categories(include_any=True)
-    
-    train_generator = utils.Dicom_Image_Generator(
-        train_df.reset_index(),
-        ycols=categories,
-        desired_size=img_size,
-        batch_size=batch_size,
-        random_transform=False,
-        rgb=rgb
-    )
+        y_pred = model.predict_proba(X)
+        fig,ax=plt.subplots(1,2,figsize=(10,10))
+        ax[0].imshow(y,aspect='auto')
+        ax[1].imshow(y_pred,aspect='auto')
+        ax[0].set_title('y_actual')
+        ax[1].set_title('y_pred')
+        fig.savefig(filename)
 
-    validate_generator = utils.Dicom_Image_Generator(
-        validate_df.reset_index(),
-        ycols=categories,
-        desired_size=img_size,
-        batch_size=batch_size,
-        random_transform=False,
-        rgb=rgb
-    )
+    def build(self):
 
+        # load training df
+        self.tdf = utils.load_training_data(self.dataloc)
 
-    # load model
-    model = models(model_name, input_image_size=img_size, number_of_output_categories=len(categories))
+        # set up training fraction
+        ## train and validate dataframes
+        shuff = self.tdf.sample(frac=self.training_fraction)
+        self.train_df = shuff.iloc[:int(0.90*len(shuff))]
+        self.validate_df = shuff.iloc[int(0.10*len(shuff)):]
+        len(shuff),len(self.train_df),len(self.validate_df)
 
-    if weights_path is not None:
-        model.load_weights(weights_path)
+        # set up generators
+        self.categories = utils.define_categories(include_any=True)
+        
+        self.train_generator = utils.Dicom_Image_Generator(
+            self.train_df.reset_index(),
+            ycols=self.categories,
+            desired_size=self.img_size,
+            batch_size=self.batch_size,
+            random_transform=False,
+            rgb=self.rgb
+        )
 
-    # train
-    for i in range(10):
+        self.validate_generator = utils.Dicom_Image_Generator(
+            self.validate_df.reset_index(),
+            ycols=self.categories,
+            desired_size=self.img_size,
+            batch_size=self.batch_size,
+            random_transform=False,
+            rgb=self.rgb
+        )
+
+        # load model
+        self.model = models(self.model_name, 
+                                input_image_size=self.img_size, 
+                                number_of_output_categories=len(self.categories))
+
+        if self.weights_path is not None:
+            self.model.load_weights(self.weights_path)
+
+    def train(self):
         try:
-            model.fit_generator(
-                generator=train_generator,
-                steps_per_epoch=len(train_df)//batch_size,
-                validation_data=validate_generator,
-                validation_steps=len(validate_df)//batch_size,
-                epochs=1
+            self.model.fit_generator(
+                generator=self.train_generator,
+                steps_per_epoch=len(self.train_df)//self.batch_size,
+                validation_data=self.validate_generator,
+                validation_steps=len(self.validate_df)//self.batch_size,
+                epochs=self.epochs
             )
             
             datestamp = str(datetime.datetime.now()).replace(':','_').replace(' ','T')
-            model.save_weights("../untracked_files/model_weights_6_outputs_iteration={}_{}.h5".format(i,datestamp))
+            self.model.save_weights("../untracked_files/model_weights_6_outputs_iteration={}.h5".format(datestamp))
             y_image_filename = '../untracked_files/y_plot_validate_{}.png'.format(datestamp)
-            make_y_image(validate_generator,model,y_image_filename)
+            self.make_y_image(self.validate_generator,model,y_image_filename)
         except Exception as e:
             print(e)
             datestamp = str(datetime.datetime.now()).replace(':','_').replace(' ','T')
-            model.save_weights("../untracked_files/model_weights_6_outputs_iteration_CRASH_DUMP={}_{}.h5".format(i,datestamp))
+            self.model.save_weights("../untracked_files/model_weights_6_outputs_iteration_CRASH_DUMP={}.h5".format(datestamp))
 
 if __name__ == '__main__':
-    main(
-        dataloc = '/mnt/win_f/rsna_data',
-        weights_path = "../untracked_files/model_weights_6_outputs_iteration=0_2019-10-04 05:38:23.464537.H5"
-    )
+    model = rsna_model(dataloc = '/mnt/win_f/rsna_data',
+                        weights_path = "../untracked_files/model_weights_6_outputs_iteration=0_2019-10-04 05:38:23.464537.H5")
+    model.build()
+    model.train()
